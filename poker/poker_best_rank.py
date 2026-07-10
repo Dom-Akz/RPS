9d86fc94b58a87159770c8c61bd7b4a7fccdd94b
SoufianeMoussaoui <dgiugfha098@gmail.com>
2026-07-10 15:46:08 +0100

    start new poker script

---
+++ poker/poker_best_rank.py
@@ -0,0 +1,16 @@
+from pokerlib import HandParser
+from pokerlib.enums import Rank, Suit
+
+
+RANK = {
+    10: "HIGH CARD",
+    9: "PAIR",
+    8: "TOW PAIR",
+    7: "THREE OF A KIND",
+    6: "STRIGHT",
+    5: "FLUSH",
+    4: "FULL HOUSE",
+    3: "FOUR OF A KIND",
+    2: "STRIGHT FLUSH",
+    1: "ROYAL FLUSH",
+}
