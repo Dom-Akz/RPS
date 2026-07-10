4edd5f04747bd156db89ad3f09477cbd59a7c920
SoufianeMoussaoui <dgiugfha098@gmail.com>
2026-07-10 17:57:46 +0100

    add generate all combo function and test the script

--- poker/poker_best_rank.py
+++ poker/poker_best_rank.py
@@ -1,46 +1,94 @@
 from pokerlib import HandParser
 from pokerlib.enums import Rank, Suit
-
+from itertools import combinations, product
 
 RANK = {
-    10: "HIGHCARD",
-    9: "PAIR",
-    8: "TOWPAIR",
-    7: "THREEOFAKIND",
-    6: "STRIGHT",
-    5: "FLUSH",
-    4: "FULLHOUSE",
-    3: "FOUROFAKIND",
-    2: "STRIGHTFLUSH",
-    1: "ROYALFLUSH",
+    1: "HIGHCARD",
+    2: "PAIR",
+    3: "TOWPAIR",
+    4: "THREEOFAKIND",
+    5: "STRIGHT",
+    6: "FLUSH",
+    7: "FULLHOUSE",
+    8: "FOUROFAKIND",
+    9: "STRIGHTFLUSH",
+    10: "ROYALFLUSH",
 }
+
+STR_RANK = {
+    "2": Rank.TWO,
+    "3": Rank.THREE,
+    "4": Rank.FOUR,
+    "5": Rank.FIVE,
+    "6": Rank.SIX,
+    "7": Rank.SEVEN,
+    "8": Rank.EIGHT,
+    "9": Rank.NINE,
+    "T": Rank.TEN,
+    "J": Rank.JACK,
+    "Q": Rank.QUEEN,
+    "K": Rank.KING,
+    "A": Rank.ACE,
+}
+
+STR_SUIT = {
+    "s": Suit.SPADE,
+    "c": Suit.CLUB,
+    "d": Suit.DIAMOND,
+    "h": Suit.HEART,
+}
+
+
+# generate all possible combos exluding the board card
+def get_all_combos(board):
+    board_set = set(board)
+    remaining_deck = [c for c in product(Rank, Suit) if c not in board_set]
+    return tuple(combinations(remaining_deck, 2))
 
 
 # return the max rank a hand can have base on the board (string)
 def max_rank(board):
-    pass
+    max = 1  # min hand rank
+
+    combos = get_all_combos(board)
+
+    # test each hand combo with the bord
+    for cm in combos:
+        hand = HandParser(list(cm) + board)
+        for k, v in RANK.items():
+            if hand.handenum.name == v:
+                if max < k:
+                    max = k
+                    break
+
+    return RANK.get(max)
 
 
 # validate the input card
 def _iscardvalid(card):
-    pass
+    rank, suit = card.split(",")
+    if rank in STR_RANK and suit in STR_SUIT:
+        return True
+    return False
 
 
 def main():
+    print("Card form: rank,suit (ex 2,s or J,h), note that 10 is represent as T")
     # get the borad card (5)
     nb_card = 0
     card = ""
+    board = []
     while nb_card < 5:
-        card = input(f"Enter card {card}: ")
+        card = input(f"Enter card {nb_card + 1}: ")
         if not _iscardvalid(card):
             print(f"Invalid card: {card}")
             exit(1)
         else:
-            pass
-            # add the card to the board
+            rank, suit = card.split(",")
+            board.append((STR_RANK[rank], STR_SUIT[suit]))
             nb_card += 1
 
-    print(max_rank)
+    print(max_rank(board))
 
 
 if __name__ == "__main__":
